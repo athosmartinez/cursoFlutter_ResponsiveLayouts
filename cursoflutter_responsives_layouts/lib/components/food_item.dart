a252d97ef1fc5ed7ad07af60140e8d45d8ac4ad0
Athos Martinez <athosandrade04@gmail.com>
2024-05-01 16:20:27 -0300

    .

--- cursoflutter_responsives_layouts/lib/components/food_item.dart
+++ cursoflutter_responsives_layouts/lib/components/food_item.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 
 class FoodItem extends StatelessWidget {
-const FoodItem({ Key? key, required this.itemTitle, required this.itemPrice, required this.imageURI }) : super(key: key);
+  const FoodItem(
+      {Key? key,
+      required this.itemTitle,
+      required this.itemPrice,
+      required this.imageURI})
+      : super(key: key);
   final String itemTitle;
   final String itemPrice;
   final String imageURI;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return InkWell(
       child: Card(
         clipBehavior: Clip.hardEdge,
         color: Theme.of(context).colorScheme.surfaceVariant,
         elevation: 0,
         child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Padding(
@@ -21,14 +27,20 @@ const FoodItem({ Key? key, required this.itemTitle, required this.itemPrice, req
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(itemTitle, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),),
-                  Text("R\$ " + itemPrice),
+                  Text(
+                    itemTitle,
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                  Text("R\$ $itemPrice"),
                 ],
               ),
             ),
             Image(
               image: AssetImage(imageURI),
               fit: BoxFit.cover,
+              height: 85,
+              width: 85,
             ),
           ],
         ),
